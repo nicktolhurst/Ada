2caa46ec74183c4fb1874b9ebbac15f5b285c3f3
Nick Tolhurst <Nick.Tolhurst@drdoctor.co.uk>
2024-07-11 20:43:39 +0100

    init commit
    clones echo bot
    changes name to adabot
    updates .NET version
    updates packages

---
+++ AdapterWithErrorHandler.cs
@@ -0,0 +1,26 @@
+﻿// Generated with EchoBot .NET Template version v4.22.0
+
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace AdaBot
+{
+    public class AdapterWithErrorHandler : CloudAdapter
+    {
+        public AdapterWithErrorHandler(BotFrameworkAuthentication auth, ILogger<IBotFrameworkHttpAdapter> logger)
+            : base(auth, logger)
+        {
+            OnTurnError = async (turnContext, exception) =>
+            {
+                logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
+
+                await turnContext.SendActivityAsync("The bot encountered an error or bug.");
+                await turnContext.SendActivityAsync("To continue to run this bot, please fix the bot source code.");
+
+                await turnContext.TraceActivityAsync("OnTurnError Trace", exception.Message, "https://www.botframework.com/schemas/error", "TurnError");
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+﻿# AdaBot
+
+## Prerequisites
+
+- [.NET SDK](https://dotnet.microsoft.com/download) version 8.0
+
+  ```bash
+  # determine dotnet version
+  dotnet --version
+  ```
+
+## Run Locally
+
+```bash
+# run the bot
+dotnet run
+```
+
+## Testing the bot using Bot Framework Emulator
+
+[Bot Framework Emulator](https://github.com/microsoft/botframework-emulator) is a desktop application that allows bot developers to test and debug their bots on localhost or running remotely through a tunnel.
+
+- Install the Bot Framework Emulator version 4.9.0 or greater from [here](https://github.com/Microsoft/BotFramework-Emulator/releases)
+
+### Connect to the bot using Bot Framework Emulator
+
+- Launch Bot Framework Emulator
+- File -> Open Bot
+- Enter a Bot URL of `http://localhost:3978/api/messages`
